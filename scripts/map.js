038df0e98f009cc59db6c4598927e71d9c7eeca0
hanshenrik <hh.gronsleth@gmail.com>
2017-05-09 23:52:37 +0200

    Use mandaljazz.no for artists events without dedicated page

--- scripts/map.js
+++ scripts/map.js
@@ -94,7 +94,7 @@ function addEventsToMap() {
 
     eventFeature.properties.title = eventFeature.properties.artist + ' @ ' + eventFeature.properties.place;
     eventFeature.properties.imageURL = eventFeature.properties.image ? '../images/artists/' + eventFeature.properties.image + '.jpg' : '../images/artists/' + defaultImages[i % defaultImages.length] + '.jpg';
-    eventFeature.properties.mandaljazzURL = 'http://mandaljazz.no/artister/' + eventFeature.properties.image;
+    eventFeature.properties.mandaljazzURL = eventFeature.properties.image ? 'http://mandaljazz.no/artister/' + eventFeature.properties.image : 'http://mandaljazz.no/';
 
     markerDiv.style.backgroundImage = 'url(' + eventFeature.properties.imageURL + ')';
     markerDiv.innerHTML = "\
