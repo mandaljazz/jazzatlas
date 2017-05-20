2be0bced16cde92326a08ce9ec1d4bd6c3591976
hanshenrik <hh.gronsleth@gmail.com>
2017-05-20 16:36:04 +0200

    Handle address and bookmark bars on mobile causing height issues, handle resize (orientation flip on mobile)

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,14 @@ map.on('load', function() {
 });
 
 $(document).ready(function() {
+  // Workaround for iOS and Chrome on mobile to take address and bookmark bars into account
+  $('#map').css({ height: window.innerHeight });
+  // In case user flips orientation or resizes the screen, handle it
+  window.addEventListener("resize", function() {
+    $('#map').css({ height: window.innerHeight });
+    map.resize();
+  }, false);
+
   $('#play-button').click(function() {
     togglePlayback();
   })
